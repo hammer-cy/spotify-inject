a89c57d36743722e2e6b7b4f9d785398d3b07286
hammer-cy <h4mmer7ime@gmail.com>
2021-04-26 01:17:18 +0000

    init

---
+++ inject.py
@@ -0,0 +1,36 @@
+#! /bin/python
+
+import requests
+import asyncio
+import websockets
+import json
+
+data = requests.get('http://localhost:9222/json').json()
+ws_uri = data[0]['webSocketDebuggerUrl']
+
+script = """
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'http://website.com/css/stylesheet.css';
+    link.media = 'all';
+    head.appendChild(link);
+}"""
+
+payload = {
+    'id': 69420,
+    'method': 'Runtime.evaluate',
+    'params': {'expression': script}}
+
+async def inject(uri):
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(json.dumps(payload))
+        await websocket.recv()
+
+asyncio.get_event_loop().run_until_complete(
+    inject(ws_uri))
